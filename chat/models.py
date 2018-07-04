53a0346f965a3be53a7def80ab5ca564476a895a
Alexey Tsivunin <most-208@yandex.ru>
2018-07-04 12:44:11 +0300

    + register and login

--- chat/models.py
+++ chat/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.TextField(max_length=50)
+    name = models.TextField(max_length=50, unique=True)
     password = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Message(models.Model):
@@ -12,3 +15,6 @@ class Message(models.Model):
     time = models.TimeField()
     user_id = models.PositiveIntegerField()
     text = models.TextField()
+
+    def __str__(self):
+        return "{} {} {}".format(self.user_id, self.time, self.date)
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
 from django.shortcuts import redirect, render
-from django.views.decorators.csrf import csrf_protect
+
+from .models import User
 
 
 def index(request):
@@ -7,14 +10,34 @@ def index(request):
 
 
 def login(request):
-    context = {
-        "username": request.POST["username"],
-        "password": request.POST["password"]
-    } if request.method == "POST" else None
-    return render(request, "chat/test.html", context=context)
+    if request.method == "POST":
+        try:
+            User.objects.get(name=request.POST["username"],
+                               password=request.POST["password"])
+        except ObjectDoesNotExist:
+            context = {"error_message": "Wrong username or password."}
+            return render(request, "chat/login.html", context=context)
+        else:
+            context = {"username": request.POST["username"],
+                       "password": request.POST["password"]}
+            return render(request, "chat/test.html", context=context)
+    return render(request, "chat/login.html", context=dict())
 
 
 def register(request):
+    context = dict()
     if request.method == "POST":
-        pass
-    return render(request, "chat/register.html", context=dict())
+        if request.POST["password"] != request.POST["confirm_password"]:
+            context["error_message"] = "Passwords are different."
+            return render(request, "chat/register.html", context=context)
+        try:
+            User(name=request.POST["username"],
+                 password=request.POST["password"]).save()
+        except IntegrityError:
+            context["error_message"] = "This username already exists."
+            return render(request, "chat/register.html", context=context)
+        else:
+            context["success_message"] = "{} have been registered!"\
+                .format(request.POST["username"])
+            return render("chat/register.html", context=context)
+    return render(request, "chat/register.html", context=context)
