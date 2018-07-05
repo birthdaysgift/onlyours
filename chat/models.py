0fb14dda6a74415230912e78eacbd4677e84718f
Alexey Tsivunin <most-208@yandex.ru>
2018-07-05 10:11:28 +0300

    + chat window + logout option + user cant send empty message

--- chat/models.py
+++ chat/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField(max_length=50, unique=True)
+    name = models.TextField(max_length=50, primary_key=True)
     password = models.TextField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class Message(models.Model):
+class Message2All(models.Model):
     date = models.DateField()
     time = models.TimeField()
-    user_id = models.PositiveIntegerField()
-    text = models.TextField()
+    name = models.TextField(max_length=50)
+    # blank=false doesnt work, dont know why
+    text = models.TextField(blank=False)
 
     def __str__(self):
-        return "{} {} {}".format(self.user_id, self.time, self.date)
+        return "{} {} {}".format(self.name, self.date, self.time)
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,29 @@
+import time
+
 from django.core.exceptions import ObjectDoesNotExist
+
 from django.db import IntegrityError
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from .models import User
+from .models import User, Message2All
+
+
+def chat(request):
+    if "username" not in request.session:
+        return redirect(reverse("Login page name"))
+    messages = Message2All.objects.all()
+    if request.method == "POST" and request.POST["message"]:
+        context = {"message": request.POST["message"]}
+        msg_datetime = time.gmtime(time.time())
+        msg_date = time.strftime("%Y-%m-%d", msg_datetime)
+        msg_time = time.strftime("%X", msg_datetime)
+        name = request.session["username"]
+        text = request.POST["message"]
+        Message2All(date=msg_date, time=msg_time, name=name, text=text).save()
+
+        return redirect("Chat page name")
+    return render(request, "chat/chat.html", context={"messages": messages})
 
 
 def index(request):
@@ -13,15 +34,19 @@ def login(request):
     if request.method == "POST":
         try:
             User.objects.get(name=request.POST["username"],
-                               password=request.POST["password"])
+                             password=request.POST["password"])
         except ObjectDoesNotExist:
             context = {"error_message": "Wrong username or password."}
             return render(request, "chat/login.html", context=context)
         else:
-            context = {"username": request.POST["username"],
-                       "password": request.POST["password"]}
-            return render(request, "chat/test.html", context=context)
-    return render(request, "chat/login.html", context=dict())
+            request.session["username"] = request.POST["username"]
+            return redirect(reverse("Chat page name"))
+    return render(request, "chat/login.html")
+
+
+def logout(request):
+    del request.session["username"]
+    return redirect(reverse("Login page name"))
 
 
 def register(request):
@@ -37,7 +62,7 @@ def register(request):
             context["error_message"] = "This username already exists."
             return render(request, "chat/register.html", context=context)
         else:
-            context["success_message"] = "{} have been registered!"\
+            context["success_message"] = "{} have been registered!" \
                 .format(request.POST["username"])
-            return render("chat/register.html", context=context)
+            return render(request, "chat/register.html", context=context)
     return render(request, "chat/register.html", context=context)
