87c5671cd4906c6d69c6b19cc8a75a1401301a21
Alexey Tsivunin <most-208@yandex.ru>
2018-08-26 09:43:17 +0300

    Added files to previous commit.

--- onlyours/urls.py
+++ onlyours/urls.py
@@ -1,9 +1,14 @@
+from django.views.generic.base import RedirectView
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
-    path("", include("auth_custom.urls")),
-    path("", include("pages.urls")),
-    path("", include("talks.urls")),
-    path('admin/', admin.site.urls)
+    path("", RedirectView.as_view(url=reverse_lazy("talks:talk", kwargs={
+        "receiver_name": "global",
+        "page_num": 1
+    })), name="index"),
+    path('admin/', admin.site.urls),
+    path("auth/", include("auth_custom.urls")),
+    path("pages/", include("pages.urls")),
+    path("talks/", include("talks.urls"))
 ]
