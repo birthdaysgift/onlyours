17966cf5e6692a6b43fa9615bb1928345537c7e8
Alexey Tsivunin <most-208@yandex.ru>
2018-11-14 12:00:47 +0300

    Added validation exceptions into aligned_range_of_pages.

--- talks/utils.py
+++ talks/utils.py
@@ -2,8 +2,14 @@ import math
 
 
 def aligned_range_of_pages(page=1, range_length=5, last_page=10):
+    if page < 1:
+        raise ValueError("`page` can't be less than 1")
+    if page > last_page:
+        raise ValueError("`page` can't be greater than `last_page`")
     middle = math.ceil(range_length / 2)
     delta = range_length - middle
+    if page > last_page:
+        raise[]
     if last_page <= range_length:
         a = 1
         b = last_page
