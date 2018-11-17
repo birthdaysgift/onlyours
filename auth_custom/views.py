1df752052fffb259c58059d54f780ac08b76c161
Alexey Tsivunin <most-208@yandex.ru>
2018-11-17 06:49:29 +0300

    Added success message to login view after success registration.

--- auth_custom/views.py
+++ auth_custom/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import views as auth_views
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
@@ -10,14 +12,25 @@ class LoginView(auth_views.LoginView):
     template_name = "auth_custom/login.html"
     redirect_authenticated_user = False
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        status_messages = messages.get_messages(self.request)
+        context_data.update({
+            'status_messages': status_messages
+        })
+        return context_data
+
 
 class LogoutView(auth_views.LogoutView):
     next_page = reverse_lazy("auth_custom:login")
 
 
-class RegisterView(CreateView):
+class RegisterView(SuccessMessageMixin, CreateView):
     form_class = RegisterForm
     template_name = "auth_custom/register.html"
     template_name_suffix = ""
-    success_url = reverse_lazy("talks:talk", kwargs={"receiver_name": "global",
-                                                     "page_num": 1})
+    success_url = reverse_lazy('auth_custom:login')
+
+    def get_success_message(self, cleaned_data):
+        return f'User {cleaned_data["username"]} was successfully created!'
+
