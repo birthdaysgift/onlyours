dda54f242c82e97c5af5bb0487267a425eb84b5f
Alexey Tsivunin <most-208@yandex.ru>
2018-11-29 11:17:02 +0300

    Adding to previous commit
    This commit adds changes to https://github.com/birthdaysgift/onlyours/commit/5603ad4b47a8729423889ba302e283f414017408

--- pages/signals.py
+++ pages/signals.py
@@ -18,7 +18,7 @@ def delete_unused_photo(sender, **kwargs):
 def delete_unused_video(sender, **kwargs):
     deleted_uservideo = kwargs["instance"]
     if not sender.objects.filter(video=deleted_uservideo.video):
-        video = Video.objects.get(file=deleted_uservideo.video.video)
+        video = Video.objects.get(file=deleted_uservideo.video.file)
         video.delete()
         try:
             os.remove(os.path.join(settings.MEDIA_ROOT, video.file.name))
