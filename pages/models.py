20e18e5244da33665b9d7287ecedf38a8bbb0bb7
Alexey Tsivunin <most-208@yandex.ru>
2018-12-06 10:55:50 +0300

    Reseted previous silly commit

--- pages/models.py
+++ pages/models.py
@@ -41,8 +41,6 @@ class PostDislike(models.Model):
 class Photo(models.Model):
     file = models.ImageField()
     thumbnail = models.ImageField(default='')
-    date = models.DateField(auto_now_add=True)
-    time = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.file.name
@@ -77,8 +75,6 @@ class UserPhoto(models.Model):
 class Video(models.Model):
     file = models.FileField()
     thumbnail = models.ImageField(default='')
-    date = models.DateField(auto_now_add=True)
-    time = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.file.name
