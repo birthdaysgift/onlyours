5e9aa9fbdb0c8a24e4751bc71a46af3d34e0a770
Alexey Tsivunin <most-208@yandex.ru>
2018-12-18 18:58:03 +0300

    Added leafing through photos by left and right arrow keys

--- photos/static/photos/js/ajax/all_photos.js
+++ photos/static/photos/js/ajax/all_photos.js
@@ -16,6 +16,7 @@ $(".new-photo-submit").click(function(event) {
 
 $(".photo-list-avatar").click(function(event){
     event.stopPropagation();
+    $(event.target).parent().addClass('selected');
     var url = $(event.target.previousElementSibling).attr('href');
     $.get({
         url: url,
@@ -23,6 +24,34 @@ $(".photo-list-avatar").click(function(event){
             $('.all-photos-background').prepend(htmlData);
         }
     });
+});
+
+$(document).keypress(function(event) {
+    var left_key = KeyEvent.DOM_VK_LEFT;
+    var right_key = KeyEvent.DOM_VK_RIGHT;
+    if ((event.keyCode === left_key) || (event.keyCode === right_key)) {
+        var selected_photo = $('.photo-avatar-wrapper.selected');
+        var opened_photo = $('.detail-photo-background');
+        if ((selected_photo.length === 1) && (opened_photo.length === 1)){
+            opened_photo.remove();
+            selected_photo.removeClass('selected');
+            if (event.keyCode === right_key) {
+                var next_photo = selected_photo.next();
+            }
+            if (event.keyCode === left_key) {
+                var next_photo = selected_photo.prev();
+            }
+            var url = next_photo.find('.detail-photo-link').attr('href');
+            $.get({
+                url: url,
+                success: function(htmlData) {
+                    next_photo.addClass('selected');
+                    $('.all-photos-background').prepend(htmlData);
+                }
+            });
+            next_photo.addClass('selected');
+        }
+    }
 });
 
 $(".new-photo-input").change(function(event) {
