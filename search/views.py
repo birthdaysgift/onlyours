dd1e4b8337dd2267715b65d4f1bee33714eae02b
Alexey Tsivunin <most-208@yandex.ru>
2018-12-24 20:10:21 +0300

    Added simple django form to search

--- search/views.py
+++ search/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from . import forms
+
 
 class SearchPageView(View):
     template_name = 'search/users.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        context = {
+            'form': forms.UserSearchForm()
+        }
+        return render(request, self.template_name, context=context)
