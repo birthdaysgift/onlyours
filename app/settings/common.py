3bb47d9c6c32f62e615232e6da2f34128b329da5
birthdaysgift <most-208@yandex.ru>
2019-03-27 12:40:11 +0300

    Deleted useless STATICFILES_DIRS setting

--- app/settings/common.py
+++ app/settings/common.py
@@ -78,10 +78,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "staticfiles"),
-)
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
