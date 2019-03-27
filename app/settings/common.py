68d0c233185dd279fae7b2c5a1bfef0b58a006b4
birthdaysgift <most-208@yandex.ru>
2019-03-27 12:46:05 +0300

    Deleted useless comments in settings

--- app/settings/common.py
+++ app/settings/common.py
@@ -7,9 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(*tj^w_*q-*s61kfcg@hy#2g&lxs-#=bz_oa@f-ci&n!b1gz&c'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = []
 
