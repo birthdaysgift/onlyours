8015f8c56f20f86eaf1ab9809a2d72f261b0f0d2
birthdaysgift <most-208@yandex.ru>
2019-03-29 20:22:43 +0300

    Added upload_to and blank params to User.avatar field

--- app/auth_custom/models.py
+++ app/auth_custom/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 def user_photos_path(instance, filename):
-    return f"users/{instance.username}/photos/{filename}"
+    return f'photos/{instance.username}/{filename}'
 
 
 class User(auth_models.AbstractUser):
     objects = auth_models.UserManager()
-    avatar = models.ImageField()
+    avatar = models.ImageField(upload_to=user_photos_path, blank=True)
     status = models.CharField(max_length=1000, blank=True, default="")
     city = models.CharField(max_length=50, blank=True, default="")
     birthday = models.DateField(blank=True, null=True)
