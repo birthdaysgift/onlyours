ad1726c16dbcc2d88cd72a1545a29304ff9a72f2
birthdaysgift <most-208@yandex.ru>
2019-04-12 22:48:07 +0300

    Changed upload_to in auth_custom.User.avatar

--- app/auth_custom/models.py
+++ app/auth_custom/models.py
@@ -9,7 +9,7 @@ def user_photos_path(instance, filename):
 
 class User(auth_models.AbstractUser):
     objects = auth_models.UserManager()
-    avatar = models.ImageField(upload_to=user_photos_path, blank=True)
+    avatar = models.ImageField(upload_to='photos', blank=True)
     status = models.CharField(max_length=1000, blank=True, default="")
     city = models.CharField(max_length=50, blank=True, default="")
     birthday = models.DateField(blank=True, null=True)
