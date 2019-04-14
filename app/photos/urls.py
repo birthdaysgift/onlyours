4f2c9c4af91f3ef81c0297a88cb250caf6d838c2
birthdaysgift <most-208@yandex.ru>
2019-04-14 23:15:05 +0300

    Changed CBVs to FBVs in photos.views
    For simplicity

--- app/photos/urls.py
+++ app/photos/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'photos'
 
 urlpatterns = [
-    path('', views.PhotosListView.as_view(), name='all'),
-    path('add/', views.AddPhotoView.as_view(), name='add'),
-    path('<int:posted_photo_id>/', views.DetailPhotoView.as_view(), name='detail'),
-    path('<int:posted_photo_id>/delete/', views.DeletePhotoView.as_view(), name='delete'),
+    path('', views.all_photos, name='all'),
+    path('add/', views.add_photo, name='add'),
+    path('<int:posted_photo_id>/', views.detail_photo, name='detail'),
+    path('<int:posted_photo_id>/delete/', views.delete_photo, name='delete'),
     path('<int:posted_photo_id>/like/', views.like_photo, name='like'),
     path('<int:posted_photo_id>/dislike/', views.dislike_photo, name='dislike'),
 ]
--- app/photos/views.py
+++ app/photos/views.py
@@ -2,77 +2,68 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.utils.text import get_valid_filename
-from django.views import View
 
 from auth_custom.models import User
 from .forms import AddPhotoForm
 from .models import PostedPhoto, Photo
 
 
-class DetailPhotoView(View):
+def detail_photo(request, username=None, posted_photo_id=None):
     template_name = 'photos/ajax/detail_photo.html'
+    if request.is_ajax():
+        posted_photo = get_object_or_404(PostedPhoto, id=posted_photo_id)
 
-    def get(self, request, username=None, posted_photo_id=None):
-        if request.is_ajax():
-            posted_photo = get_object_or_404(PostedPhoto, id=posted_photo_id)
+        posted_photo.n_likes = posted_photo.users_who_liked.count()
+        posted_photo.n_dislikes = posted_photo.users_who_disliked.count()
 
-            posted_photo.n_likes = posted_photo.users_who_liked.count()
-            posted_photo.n_dislikes = posted_photo.users_who_disliked.count()
+        posted_photo.is_liked = posted_photo.liked_by(request.user)
+        posted_photo.is_disliked = posted_photo.disliked_by(request.user)
 
-            posted_photo.is_liked = posted_photo.liked_by(request.user)
-            posted_photo.is_disliked = posted_photo.disliked_by(request.user)
-
-            context = {'posted_photo': posted_photo}
-            return render(request, self.template_name, context=context)
-        url = reverse('pages:page', kwargs={'username': username})
-        return redirect(url)
+        context = {'posted_photo': posted_photo}
+        return render(request, template_name, context=context)
+    url = reverse('pages:page', kwargs={'username': username})
+    return redirect(url)
 
 
-class PhotosListView(View):
+def all_photos(request, username=None):
     template_name = "photos/ajax/all_photos.html"
-
-    def get(self, request, username=None):
-        if request.is_ajax():
-            page_owner = get_object_or_404(User, username=username)
-            posted_photos = page_owner.get_posted_photos()
-            context = {
-                "posted_photos": posted_photos,
-                "page_owner": page_owner,
-                "photo_form": AddPhotoForm()
-            }
-            return render(request, self.template_name, context=context)
-        url = reverse('pages:page', kwargs={'username': username})
-        return redirect(url)
-
-
-class AddPhotoView(View):
-    template_name = 'photos/ajax/all_photos.html'
-
-    def post(self, request, username=None):
-        form = AddPhotoForm(request.POST, request.FILES)
+    if request.is_ajax():
         page_owner = get_object_or_404(User, username=username)
-        if request.user == page_owner and form.is_valid():
-            form.save()
+        posted_photos = page_owner.get_posted_photos()
+        context = {
+            "posted_photos": posted_photos,
+            "page_owner": page_owner,
+            "photo_form": AddPhotoForm()
+        }
+        return render(request, template_name, context=context)
+    url = reverse('pages:page', kwargs={'username': username})
+    return redirect(url)
 
-            filename = get_valid_filename(form.cleaned_data['file'].name)
-            photo = get_object_or_404(Photo, file=filename)
 
-            user = get_object_or_404(User, username=username)
-            PostedPhoto(user=user, photo=photo).save()
-        url = reverse('pages:page', kwargs={'username': username})
-        return redirect(url)
+def add_photo(request, username=None):
+    form = AddPhotoForm(request.POST, request.FILES)
+    page_owner = get_object_or_404(User, username=username)
+    if request.user == page_owner and form.is_valid():
+        form.save()
+
+        filename = get_valid_filename(form.cleaned_data['file'].name)
+        photo = get_object_or_404(Photo, file=filename)
+
+        user = get_object_or_404(User, username=username)
+        PostedPhoto(user=user, photo=photo).save()
+    url = reverse('pages:page', kwargs={'username': username})
+    return redirect(url)
 
 
-class DeletePhotoView(View):
-    def get(self, request, username=None, posted_photo_id=None):
-        page_owner = get_object_or_404(User, username=username)
-        if request.user == page_owner:
-            try:
-                PostedPhoto.objects.get(id=posted_photo_id).delete()
-            except PostedPhoto.DoesNotExist:
-                pass
-        url = reverse("pages:page", kwargs={"username": username})
-        return redirect(url)
+def delete_photo(request, username=None, posted_photo_id=None):
+    page_owner = get_object_or_404(User, username=username)
+    if request.user == page_owner:
+        try:
+            PostedPhoto.objects.get(id=posted_photo_id).delete()
+        except PostedPhoto.DoesNotExist:
+            pass
+    url = reverse("pages:page", kwargs={"username": username})
+    return redirect(url)
 
 
 def like_photo(request, username=None, posted_photo_id=None):
