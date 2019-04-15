520c94da6a17a4e7bcbd47b70d1205cc5731888e
birthdaysgift <most-208@yandex.ru>
2019-04-15 20:29:47 +0300

    Changed CBVs to FBVs in videos.views

--- app/videos/urls.py
+++ app/videos/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'videos'
 
 urlpatterns = [
-    path('', views.VideosListView.as_view(), name='all'),
-    path('add/', views.AddVideoView.as_view(), name='add'),
-    path('<int:uservideo_id>/', views.DetailVideoView.as_view(), name='detail'),
-    path('<int:uservideo_id>/delete/', views.DeleteVideoView.as_view(), name='delete'),
-    path('<int:uservideo_id>/like/', views.LikeVideoView.as_view(), name='like'),
-    path('<int:uservideo_id>/dislike/', views.DislikeVideoView.as_view(), name='dislike')
+    path('', views.all_videos, name='all'),
+    path('add/', views.add_video, name='add'),
+    path('<int:uservideo_id>/', views.detail_video, name='detail'),
+    path('<int:uservideo_id>/delete/', views.delete_video, name='delete'),
+    path('<int:uservideo_id>/like/', views.like_video, name='like'),
+    path('<int:uservideo_id>/dislike/', views.dislike_video, name='dislike')
 ]
 
--- app/videos/views.py
+++ app/videos/views.py
@@ -3,7 +3,6 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.utils.text import get_valid_filename
-from django.views import View
 
 from auth_custom.models import User
 
@@ -11,114 +10,105 @@ from .forms import AddVideoForm
 from .models import UserVideo, Video, VideoDislike, VideoLike
 
 
-class DetailVideoView(View):
+def detail_video(request, username=None, uservideo_id=None):
     template_name = 'videos/ajax/detail_video.html'
-
-    def get(self, request, username=None, uservideo_id=None):
-        if request.is_ajax():
-            uservideo = get_object_or_404(UserVideo, id=uservideo_id)
-            likes = VideoLike.objects.filter(uservideo=uservideo)
-            dislikes = VideoDislike.objects.filter(uservideo=uservideo)
-            setattr(uservideo, 'likes', likes)
-            setattr(uservideo, 'dislikes', dislikes)
-            is_liked = VideoLike.objects.filter(
-                uservideo=uservideo, user=request.user
-            ).exists()
-            is_disliked = VideoDislike.objects.filter(
-                uservideo=uservideo, user=request.user
-            ).exists()
-            setattr(uservideo, 'is_liked', is_liked)
-            setattr(uservideo, 'is_disliked', is_disliked)
-            context = {'uservideo': uservideo}
-            return render(request, self.template_name, context=context)
-        raise Http404()
+    if request.is_ajax():
+        uservideo = get_object_or_404(UserVideo, id=uservideo_id)
+        likes = VideoLike.objects.filter(uservideo=uservideo)
+        dislikes = VideoDislike.objects.filter(uservideo=uservideo)
+        setattr(uservideo, 'likes', likes)
+        setattr(uservideo, 'dislikes', dislikes)
+        is_liked = VideoLike.objects.filter(
+            uservideo=uservideo, user=request.user
+        ).exists()
+        is_disliked = VideoDislike.objects.filter(
+            uservideo=uservideo, user=request.user
+        ).exists()
+        setattr(uservideo, 'is_liked', is_liked)
+        setattr(uservideo, 'is_disliked', is_disliked)
+        context = {'uservideo': uservideo}
+        return render(request, template_name, context=context)
+    raise Http404()
 
 
-class VideosListView(View):
+def all_videos(request, username=None):
     template_name = "videos/ajax/all_videos.html"
-
-    def get(self, request, username=None):
-        if request.is_ajax():
-            page_owner = get_object_or_404(User, username=username)
-            user_videos = UserVideo.objects.filter(user=page_owner)
-            user_videos = user_videos.select_related("video")
-            context = {
-                "user_videos": user_videos,
-                "page_owner": page_owner,
-                "video_form": AddVideoForm()
-            }
-            return render(request, self.template_name, context=context)
-        raise Http404()
+    if request.is_ajax():
+        page_owner = get_object_or_404(User, username=username)
+        user_videos = UserVideo.objects.filter(user=page_owner)
+        user_videos = user_videos.select_related("video")
+        context = {
+            "user_videos": user_videos,
+            "page_owner": page_owner,
+            "video_form": AddVideoForm()
+        }
+        return render(request, template_name, context=context)
+    raise Http404()
 
 
-class AddVideoView(View):
+def add_video(request, username=None):
     template_name = "videos/ajax/add_video.html"
+    page_owner = get_object_or_404(User, username=username)
+    form = AddVideoForm(request.POST, request.FILES)
+    if request.user == page_owner and form.is_valid():
+        form.save()
 
-    def post(self, request, username=None):
-        page_owner = get_object_or_404(User, username=username)
-        form = AddVideoForm(request.POST, request.FILES)
-        if request.user == page_owner and form.is_valid():
-            form.save()
+        filename = get_valid_filename(form.cleaned_data['file'].name)
+        video = get_object_or_404(Video, file=filename)
+        user = get_object_or_404(User, username=username)
+        UserVideo(user=user, video=video).save()
 
-            filename = get_valid_filename(form.cleaned_data['file'].name)
-            video = get_object_or_404(Video, file=filename)
-            user = get_object_or_404(User, username=username)
-            UserVideo(user=user, video=video).save()
-
-            kwargs = {'username': username}
-        url = reverse('pages:page', kwargs=kwargs)
-        return redirect(url)
+        kwargs = {'username': username}
+    url = reverse('pages:page', kwargs=kwargs)
+    return redirect(url)
 
 
-class DeleteVideoView(View):
-    def get(self, request, username=None, uservideo_id=None):
-        page_owner = get_object_or_404(User, username=username)
-        if request.user == page_owner:
-            try:
-                UserVideo.objects.get(id=uservideo_id).delete()
-            except UserVideo.DoesNotExist:
-                pass
-        url = reverse("pages:page", kwargs={"username": username})
-        return redirect(url)
+def delete_video(request, username=None, uservideo_id=None):
+    page_owner = get_object_or_404(User, username=username)
+    if request.user == page_owner:
+        try:
+            UserVideo.objects.get(id=uservideo_id).delete()
+        except UserVideo.DoesNotExist:
+            pass
+    url = reverse("pages:page", kwargs={"username": username})
+    return redirect(url)
 
 
-class LikeVideoView(View):
-    @transaction.atomic
-    def get(self, request, username=None, uservideo_id=None):
-        if request.is_ajax():
-            uservideo = get_object_or_404(UserVideo, id=uservideo_id)
-            like = VideoLike.objects.filter(
-                user=request.user, uservideo=uservideo
-            )
-            dislike = VideoDislike.objects.filter(
-                user=request.user, uservideo=uservideo
-            )
-            if like.exists():
-                like[0].delete()
-            else:
-                if dislike.exists():
-                    dislike[0].delete()
-                VideoLike(user=request.user, uservideo=uservideo).save()
-            return HttpResponse()
-        raise Http404()
-
-
-class DislikeVideoView(View):
-    @transaction.atomic
-    def get(self, request, username=None, uservideo_id=None):
-        if request.is_ajax():
-            uservideo = get_object_or_404(UserVideo, id=uservideo_id)
-            like = VideoLike.objects.filter(
-                user=request.user, uservideo=uservideo
-            )
-            dislike = VideoDislike.objects.filter(
-                user=request.user, uservideo=uservideo
-            )
+@transaction.atomic
+def like_video(request, username=None, uservideo_id=None):
+    if request.is_ajax():
+        uservideo = get_object_or_404(UserVideo, id=uservideo_id)
+        like = VideoLike.objects.filter(
+            user=request.user, uservideo=uservideo
+        )
+        dislike = VideoDislike.objects.filter(
+            user=request.user, uservideo=uservideo
+        )
+        if like.exists():
+            like[0].delete()
+        else:
             if dislike.exists():
                 dislike[0].delete()
-            else:
-                if like.exists():
-                    like[0].delete()
-                VideoDislike(user=request.user, uservideo=uservideo).save()
-            return HttpResponse()
-        raise Http404()
+            VideoLike(user=request.user, uservideo=uservideo).save()
+        return HttpResponse()
+    raise Http404()
+
+
+@transaction.atomic
+def dislike_video(request, username=None, uservideo_id=None):
+    if request.is_ajax():
+        uservideo = get_object_or_404(UserVideo, id=uservideo_id)
+        like = VideoLike.objects.filter(
+            user=request.user, uservideo=uservideo
+        )
+        dislike = VideoDislike.objects.filter(
+            user=request.user, uservideo=uservideo
+        )
+        if dislike.exists():
+            dislike[0].delete()
+        else:
+            if like.exists():
+                like[0].delete()
+            VideoDislike(user=request.user, uservideo=uservideo).save()
+        return HttpResponse()
+    raise Http404()
